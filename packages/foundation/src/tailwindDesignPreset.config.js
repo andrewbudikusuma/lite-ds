8cbdef552051d4e474e2f720b39e89bfb81c0ba9
Andrew Budikusuma <andrew.budikusuma@qoala.id>
2023-06-18 16:45:04 +0700

    build tokens

---
+++ packages/foundation/src/tailwindDesignPreset.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line no-undef
+const tokens = require('./tokens/js/tokens');
+
+/**
+ * Tailwind requires the values:
+ *{
+ *  "boxShadow": {
+ *    "level-1": "value",
+ *    "level-2": "...",
+ *     ....
+ *   },
+ *  "colors": {
+ *    ...
+ *  },
+ *  ....
+ * }
+ */
+const flattenValue = (obj) => {
+  const result = {};
+  Object.keys(obj).forEach((key) => {
+    // If value property does not exist
+    if (!obj[key].value) {
+      // Should go one level deeper
+      result[key] = flattenValue(obj[key]);
+    } else {
+      result[key] = obj[key].value;
+    }
+  });
+
+  return result;
+};
+
+module.exports = {
+  prefix: 'newline-',
+  theme: {
+    colors: flattenValue(tokens.colors),
+    borderRadius: flattenValue(tokens.radius),
+    spacing: flattenValue(tokens.spacings),
+    animation: flattenValue(tokens.animations),
+    boxShadow: flattenValue(tokens.shadows),
+    fontFamily: {
+      sans: ['"Inter"'],
+    },
+  },
+  // Enable only necessary plugins
+  // https://tailwindcss.com/docs/configuration#core-plugins
+  plugins: [],
+};
